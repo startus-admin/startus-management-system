7a47258c01a8574084633d401a233a90585f7882
mattun9 <hisasimatu3117@gmail.com>
2026-03-03 16:30:53 +0900

    Auto-commit: 2026-03-03_16:30:53

--- member-admin/js/classroom.js
+++ member-admin/js/classroom.js
@@ -369,9 +369,9 @@ function openClassroomForm(classroom) {
           <div class="form-group">
             <label>時間帯</label>
             <div style="display:flex;align-items:center;gap:4px">
-              <input type="time" name="time_start" value="${c.time_start || ''}" style="flex:1">
+              <input type="time" name="time_start" value="${time.start}" style="flex:1">
               <span>〜</span>
-              <input type="time" name="time_end" value="${c.time_end || ''}" style="flex:1">
+              <input type="time" name="time_end" value="${time.end}" style="flex:1">
             </div>
           </div>
           <div class="form-group" style="grid-column:1/-1">
